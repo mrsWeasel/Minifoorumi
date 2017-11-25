86098d5d857f0d4c644efafe992e69730d553b13
Laura Heino <laura@lauraheino.com>
2017-11-25 19:55:33 +0200

    Palauta juuri luotu viestiketju

--- src/main/java/tikape/minifoorumi/dao/ViestiketjuDao.java
+++ src/main/java/tikape/minifoorumi/dao/ViestiketjuDao.java
@@ -17,20 +17,37 @@ public class ViestiketjuDao extends AbstractNamedObjectDao<Viestiketju> {
         return new Viestiketju(resultSet.getInt("id"), resultSet.getString("aihe"));
     }
     
+    public Viestiketju findCreated(String aihe) throws SQLException {
+        try (Connection conn = database.getConnection()) {
+            // Haetaan viestiketju, jonka aihe täsmää ja joka ei sisällä vielä viestejä (siltä varalta,
+            // että useammalla ketjulla onkin sama aihe)
+            PreparedStatement stmt = conn.prepareStatement("SELECT * FROM Viestiketju WHERE aihe = ? "
+                    + "AND id NOT IN (SELECT * FROM Viesti)");
+            stmt.setString(1, aihe);
+            ResultSet rs = stmt.executeQuery();
+            
+            rs.next();
+            
+            Viestiketju viestiketju = new Viestiketju(rs.getInt("id"), rs.getString("aine"));
+            return viestiketju;
+        }
+    }
+    
     @Override
-    public Viestiketju save(Viestiketju viestiketju) throws SQLException {
+    public Viestiketju save(Viestiketju vk) throws SQLException {
             try (Connection conn = database.getConnection()) {
                 PreparedStatement stmt = conn.prepareStatement("INSERT INTO Viestiketju"
                     + "(aihe) "
                     + "VALUES (?)");
-                stmt.setString(1, viestiketju.getAihe());
+                stmt.setString(1, vk.getAihe());
             
                 stmt.executeUpdate();
                 stmt.close();
                 conn.close();
             
             // todo
-            return null;
+            Viestiketju viestiketju = findCreated(vk.getAihe());
+            return viestiketju;
         }
     }
 }
