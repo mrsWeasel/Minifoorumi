75774d753ac3fe61d917e1cb42abebd8ddff026a
Laura Heino <laura@lauraheino.com>
2017-11-25 21:29:59 +0200

    Viestin lähettäminen viestiketjun näkymästä

--- src/main/java/tikape/minifoorumi/MinifoorumiApplication.java
+++ src/main/java/tikape/minifoorumi/MinifoorumiApplication.java
@@ -20,22 +20,29 @@ public class MinifoorumiApplication {
         ViestiDao viestit = new ViestiDao(database, "Viesti");
         
         Spark.get("/messages", (req, res) -> {
-            // hae viestit
-            //Integer viestiketjuId = Integer.parseInt(req.params(":id"));
             HashMap map = new HashMap<>();
             map.put("viestit", viestit.findAll());
             map.put("viestiketjut", viestiketjut.findAll());
             return new ModelAndView(map, "messages");
         }, new ThymeleafTemplateEngine());
         
+        Spark.get("/messages/:id", (req, res) -> {
+           Integer viestiketjuId = Integer.parseInt(req.params(":id"));
+           HashMap map = new HashMap<>();
+           map.put("viestit", viestit.findAll()); // todo: vain tämän ketjun viestit
+           map.put("viestiketju", viestiketjut.findOne(viestiketjuId));
+           return new ModelAndView(map, "message");
+        }, new ThymeleafTemplateEngine());
+        
         Spark.post("/messages/:id", (req, res) -> {
-                java.util.Date utilAika = new java.util.Date();
-                java.sql.Date sqlAika = new java.sql.Date( utilAika.getTime() );
-                Integer viestiketjuId = Integer.parseInt(req.queryParams("viestiketju_id"));
+            java.util.Date utilAika = new java.util.Date();
+            java.sql.Date sqlAika = new java.sql.Date( utilAika.getTime() );
+            Integer viestiketjuId = Integer.parseInt(req.queryParams("viestiketju_id"));
                 
-                Viesti viesti = new Viesti(null, viestiketjuId, req.queryParams("name"), req.queryParams("content"), sqlAika);
-                viestit.save(viesti);
-                return "";
+            Viesti viesti = new Viesti(null, viestiketjuId, req.queryParams("name"), req.queryParams("content"), sqlAika);
+            viestit.save(viesti);
+            res.redirect("/messages/" + viestiketjuId);
+            return "";
         });
         
         Spark.post("/messages", (req, res) -> {
@@ -48,7 +55,7 @@ public class MinifoorumiApplication {
                 Viesti viesti = new Viesti(null, vk.getId(), req.queryParams("name"), req.queryParams("content"), sqlAika);
                 viestit.save(viesti);
                 //TODO: res.redirect("/messages/" + req.params(":id"));
-                res.redirect("/messages");
+                res.redirect("/messages/" + vk.getId());
                 return "";
         });
     }
